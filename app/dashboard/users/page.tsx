780b5f0ed3d4027b2c863a31e01ab8645aa054a6
Fahad-Farabee <smfahadfarabee@gmail.com>
2025-11-17 20:47:26 +0600

    dynamic routing

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-
+import Link from "next/link";
 const Users = () => {
   return (
     <div>
       <h1>Dashboard Users</h1>
       <ul className="mt-10">
-        <li> user 1 </li>
-        <li> user 2 </li>
-        <li> user 3 </li>
-        <li> user 4 </li>
+        <li>
+          <Link href="/dashboard/users/1">user 1</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/2">user 2</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/3">user 3</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/4">user 4</Link>
+        </li>
       </ul>
     </div>
   );
